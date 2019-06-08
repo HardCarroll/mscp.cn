8dc5e1597832167a140587a3ce964f2aab87116a
HardCarroll <phanchan_hc@163.com>
2019-06-08 09:06:06 +0800

    upload files

---
+++ index.php
@@ -0,0 +1,178 @@
+<!DOCTYPE html>
+<html lang="zh-CN">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <meta name="Keywords" content="关键词">
+  <meta name="Description" content="内容描述">
+  <title>弥尚餐饮装饰设计</title>
+  <link rel="stylesheet" href="/include/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="/include/css/shared.css">
+  <link rel="stylesheet" href="/include/css/homepage.css">
+</head>
+
+<body>
+  <section class="layer">
+    <section class="header">
+      <nav class="navbar navbar-inverse" id="navbar-mscp">
+        <div class="container-fluid">
+          <!-- Brand and toggle get grouped for better mobile display -->
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-navbar-mscp" aria-expanded="false">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">企业LOGO预留位置(300*100px)</a>
+          </div>
+          <!-- Collect the nav links, forms, and other content for toggling -->
+          <div class="collapse navbar-collapse" id="bs-navbar-mscp">
+            <ul class="nav navbar-nav navbar-right">
+              <li class="current"><a href="#">首页</a></li>
+              <li><a href="#advantage">服务优势</a></li>
+              <li><a href="#">一键报价</a></li>
+              <li><a href="#">精品案例</a></li>
+              <li><a href="#">企业介绍</a></li>
+              <li><a href="#">新闻资讯</a></li>
+              <li><a href="#">联系方式</a></li>
+            </ul>
+          </div><!-- /.navbar-collapse -->
+        </div><!-- /.container-fluid -->
+      </nav>
+    </section>
+    <section class="body">
+      <div id="banner">
+        <div id="carousel-mscp" class="carousel slide" data-ride="carousel">
+          <!-- Indicators -->
+          <ol class="carousel-indicators">
+            <li data-target="#carousel-mscp" data-slide-to="0" class="active"></li>
+            <li data-target="#carousel-mscp" data-slide-to="1"></li>
+            <li data-target="#carousel-mscp" data-slide-to="2"></li>
+          </ol>
+
+          <!-- Wrapper for slides -->
+          <div class="carousel-inner" role="listbox">
+            <div class="item active">
+              <img src="/images/banner.jpg" alt="...">
+            </div>
+            <div class="item">
+              <img src="/images/banner2.jpg" alt="...">
+            </div>
+            <div class="item">
+              <img src="/images/banner3.jpg" alt="...">
+            </div>
+          </div>
+
+          <!-- Controls -->
+          <a class="left carousel-control" href="#carousel-mscp" role="button" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="right carousel-control" href="#carousel-mscp" role="button" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
+        </div>
+      </div>
+      <div id="advantage"></div>
+      <div id="price"></div>
+      <div id="case">
+        <section class="case-hd">
+          <h2>全新餐厅案例</h2>
+          <h5>涵盖13个种类、数十种风格,您想要的我们都能做</h5>
+        </section>
+        <section class="container-fluid case-bd">
+          <ul class="row case-list">
+            <li class="col-xs-12 col-sm-6 col-md-4">
+              <a href="#">
+                <img src="/images/new-1.jpg" alt="">
+                <div class="cover">
+                  <h3 class="text-ellipsis title">剁椒鱼头</h3>
+                  <h5 class="text-ellipsis content">反复调试，确定剁椒黄金配比，超大的鱼头、鲜嫩的美味获得一致好评</h5>
+                  <span class="glyphicon glyphicon-plus"></span>
+                </div>
+              </a>
+            </li>
+            <li class="col-xs-12 col-sm-6 col-md-4">
+              <a href="#">
+                <img src="/images/new-2.jpg" alt="">
+                <div class="cover">
+                  <h3 class="text-ellipsis title">锅仔滋补羊肉</h3>
+                  <h5 class="text-ellipsis content">锅仔羊肉香味扑鼻且驱寒暖身，香在口，暖在心是深受欢迎的冬季家常菜</h5>
+                  <span class="glyphicon glyphicon-plus"></span>
+                </div>
+              </a>
+            </li>
+            <li class="col-xs-12 col-sm-6 col-md-4">
+              <a href="#">
+                <img src="/images/new-3.jpg" alt="">
+                <div class="cover">
+                  <h3 class="text-ellipsis title">开胃豆腐王</h3>
+                  <h5 class="text-ellipsis content">内酯豆腐是豆腐中的“小鲜肉”,质地细腻嫩滑,热量低,深受各路吃货喜爱</h5>
+                  <span class="glyphicon glyphicon-plus"></span>
+                </div>
+              </a>
+            </li>
+            <li class="col-xs-12 col-sm-6 col-md-4">
+              <a href="#">
+                <img src="/images/new-4.jpg" alt="">
+                <div class="cover">
+                  <h3 class="text-ellipsis title">秘制猪脚</h3>
+                  <h5 class="text-ellipsis content">猪脚十分软糯，有嚼感，丝毫没有肥腻感，软滑糯嫩、烂而不散、唇齿留香</h5>
+                  <span class="glyphicon glyphicon-plus"></span>
+                </div>
+              </a>
+            </li>
+            <li class="col-xs-12 col-sm-6 col-md-4">
+              <a href="#">
+                <img src="/images/new-5.jpg" alt="">
+                <div class="cover">
+                  <h3 class="text-ellipsis title">平锅臭桂鱼</h3>
+                  <h5 class="text-ellipsis content">鱼肉经过腌制，具有神奇般鲜嫩和口感，微辣且锅香飘逸，是下饭好选择</h5>
+                  <span class="glyphicon glyphicon-plus"></span>
+                </div>
+              </a>
+            </li>
+            <li class="col-xs-12 col-sm-6 col-md-4">
+              <a href="#">
+                <img src="/images/new-6.jpg" alt="">
+                <div class="cover">
+                  <h3 class="text-ellipsis title">香辣刨盐鱼</h3>
+                  <h5 class="text-ellipsis content">把生鱼用盐腌制，做成刨盐鱼，制作出符合湖南人的香鲜辣的香辣刨盐鱼</h5>
+                  <span class="glyphicon glyphicon-plus"></span>
+                </div>
+              </a>
+            </li>
+          </ul>
+        </section>
+        <section class="case-ft">
+          <a href="#" class="more">更多案例 >></a>
+        </section>
+      </div>
+      <div id="company">company</div>
+      <div id="news">news</div>
+      <div id="partner">partner</div>
+      <div id="contact">
+        <section class="contact-hd">
+          <img src="/images/bottom-text.png" alt="">
+        </section>
+        <section class="contact-bd container-fluid row">
+          <div class="col-xs-12 col-sm-6">left area</div>
+          <div class="col-xs-12 col-sm-6">right area</div>
+        </section>
+        <section class="contact-ft"></section>
+      </div>
+    </section>
+    <section class="footer">footer</section>
+  </section>
+
+
+
+  <script src="/include/jquery/jquery.min.js"></script>
+  <script src="/include/bootstrap/js/bootstrap.min.js"></script>
+</body>
+
+</html>
