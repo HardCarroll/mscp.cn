bf2c80bb0e03a24e403e9c23880130ed8262478c
HardCarroll <phanchan_hc@163.com>
2019-07-27 00:06:10 +0800

    opt home page

--- debug/debug.js
+++ debug/debug.js
@@ -20,7 +20,7 @@ $(function() {
   $("#fullpage").fullpage({
     navigation: true,
     navigationPosition: "left",
-    navigationTooltips: ["官网首页", "5秒就能帮您算出明细！", "您的设计够专业吗？", "这些案例能否激发您的灵感？", "术业有专攻，省时省心又省钱！", "选择我们，您会收获什么？", "您想要的，我们都有！", "一站式服务，为您保驾护航！", "多年专注，不玩套路！", "友好合作，互利共赢！", "关注我们，了解我们！"],
+    navigationTooltips: ["官网首页", "5秒就能帮您算出明细！", "您的设计够专业吗？", "这些案例能否激发您的灵感？", "术业有专攻，省时省心又省钱！", "选择我们，您会收获什么？", "资深设计师一对一服务！", "您想要的，我们都有！", "一站式服务，为您保驾护航！", "专业名师为您答疑解惑！","多年专注，不玩套路！", "友好合作，互利共赢！", "关注我们，了解我们！"],
     // showActiveTooltip: true,
     afterLoad: function(origin, destination, direction){
       $(destination.item).find(".hd h2").animate({top: 0, opacity: 1}, 600);
@@ -45,45 +45,83 @@ $(function() {
       }
       // 服务优势 #advantage
       if(destination.index === 4) {
-        $("#advantage .hd h5 span:nth-of-type(1)").delay(200).animate({opacity: 1}, 600);
-        $("#advantage .hd h5 b:nth-of-type(1)").delay(400).animate({opacity: 1}, 600);
-        $("#advantage .hd h5 span:nth-of-type(2)").delay(600).animate({opacity: 1}, 600);
-        $("#advantage .hd h5 b:nth-of-type(2)").delay(800).animate({opacity: 1}, 600);
-        $("#advantage .hd h5 span:nth-of-type(3)").delay(1000).animate({opacity: 1}, 600);
-        $("#advantage .hd h5 b:nth-of-type(3)").delay(1200).animate({opacity: 1}, 600);
-        $("#advantage .hd h5 span:nth-of-type(4)").delay(1400).animate({opacity: 1}, 600);
-        $("#advantage .hd h5 b:nth-of-type(4)").delay(1600).animate({opacity: 1}, 600);
-        $("#advantage .hd h5 span:nth-of-type(5)").delay(1800).animate({opacity: 1}, 600);
-        $("#advantage .bd").delay(2000).animate({height: "440px", opacity: 1}, 600, function() {
+        $("#advantage .hd h5 span:nth-of-type(1)").delay(600).animate({opacity: 1}, 600);
+        $("#advantage .hd h5 b:nth-of-type(1)").delay(800).animate({opacity: 1}, 600);
+        $("#advantage .hd h5 span:nth-of-type(2)").delay(1000).animate({opacity: 1}, 600);
+        $("#advantage .hd h5 b:nth-of-type(2)").delay(1200).animate({opacity: 1}, 600);
+        $("#advantage .hd h5 span:nth-of-type(3)").delay(1400).animate({opacity: 1}, 600);
+        $("#advantage .hd h5 b:nth-of-type(3)").delay(1600).animate({opacity: 1}, 600);
+        $("#advantage .hd h5 span:nth-of-type(4)").delay(1800).animate({opacity: 1}, 600);
+        $("#advantage .hd h5 b:nth-of-type(4)").delay(2000).animate({opacity: 1}, 600);
+        $("#advantage .hd h5 span:nth-of-type(5)").delay(2200).animate({opacity: 1}, 600);
+        $("#advantage .bd").delay(2200).animate({height: "440px", opacity: 1}, 600, function() {
           $("#advantage .list-item").animate({opacity: 1}, 600);
           $("#advantage .list-item .wrap").css("box-shadow", "0 0 3px 0 #FFDC90");
         });
-        $(destination.item).find(".btn-square").delay(2000).animate({top: 0, opacity: 1}, 600);
+        $(destination.item).find(".btn-square").delay(2400).animate({top: 0, opacity: 1}, 600);
       }
       // 免费服务 #service
       if(destination.index === 5) {
-        $(destination.item).find(".btn-square").delay(200).animate({top: 0, opacity: 1}, 600);
+        $(destination.item).find(".bd-list .list-item").css("transform", "translateY(0)");
+        $(destination.item).find(".bd-list .list-item:nth-child(2n-1) .txt").delay(600).animate({top: 0}, 600);
+        $(destination.item).find(".bd-list .list-item:nth-child(2n) .txt").delay(600).animate({top: "50%"}, 600);
+        $(destination.item).find(".btn-square").delay(600).animate({top: 0, opacity: 1}, 600);
       }
+      // 资深设计师一对一服务 #designer
+      if(destination.index === 6){}
       // 额外服务支持 #support
-      if(destination.index === 6) {
+      if(destination.index === 7) {
         $(destination.item).find(".bd-list .list-item").delay(200).animate({opacity: 1}, 600, function() {
           $(this).css("transform", "translateX(0)");
         });
         $(destination.item).find(".btn-square").delay(100).animate({top: 0, opacity: 1}, 600);
       }
-      // 多年专注，不玩套路
+      // 一站式服务 #slogan
       if(destination.index === 8) {
-        $(destination.item).find(".btn-square").delay(100).animate({top: 0, opacity: 1}, 600);
+        $(destination.item).find(".bd-list .list-item").each(function(i) {
+          $(this).delay(i*200+600).animate({left: 0}, 600);
+        });
+        $(destination.item).find(".ft").delay(2200).animate({top: 0, opacity: 1}, 600);
       }
-      // 多年专注，不玩套路
+      // 名师在线答疑 #focus
       if(destination.index === 9) {
-        $(destination.item).find(".btn-square").delay(100).animate({top: 0, opacity: 1}, 600);
+        $(destination.item).find(".bd img").delay(200).animate({opacity: 0.3}, 1000);
+        $(destination.item).find(".bd .bd-list.left li").each(function(i) {
+          $(this).delay(i*200).animate({right: 0, opacity: 1}, 600);
+        });
+        $(destination.item).find(".bd .bd-list.right li").each(function(i) {
+          $(this).delay(200*i+100).animate({left: 0, opacity: 1}, 600);
+        });
+        $(destination.item).find(".bd .bd-list.left li").delay(200).animate({right: 0, opacity: 1}, 600);
+        $(destination.item).find(".bd .bd-list.right li").delay(200).animate({left: 0, opacity: 1}, 600);
+      }
+      // 多年专注，不玩套路 #goal
+      if(destination.index === 10) {
+        $(destination.item).find(".bd-list").delay(800).animate({height: "265px"}, 600);
+        $(destination.item).find(".btn-square").delay(800).animate({top: 0, opacity: 1}, 600);
+      }
+      // 友好合作，互利共赢 #partner
+      if(destination.index === 11) {
+        $(destination.item).find(".bd-list").delay(600).animate({opacity: 1}, 1000);
+        $(destination.item).find(".btn-square").delay(600).animate({top: 0, opacity: 1}, 600);
+      }
+      // #about
+      if(destination.index === 12) {
+        $(destination.item).find(".cpy-name span:nth-child(1)").animate({top: 0, opacity: 1}, 600, function() {
+          $(this).css("transform", "translateY(-25%)");
+        });
+        $(destination.item).find(".cpy-name span:nth-child(2)").animate({top: 0, opacity: 1}, 600, function() {
+          $(this).css("transform", "translateY(25%)");
+        });
+        $(destination.item).find(".cpy-des span").each(function(i) {
+          $(this).delay(i*200+600).animate({top: 0, opacity: 1}, 1000);
+        });
       }
 
       // 离开页
-      $(origin.item).find(".hd h2").delay(700).animate({top: "-360%", opacity: 0}, 300);
-      $(origin.item).find(".hd h5 .left").delay(700).animate({left: "-360%", opacity: 0}, 300);
-      $(origin.item).find(".hd h5 .right").delay(700).animate({right: "-360%", opacity: 0}, 300);
+      $(origin.item).find(".hd h2").animate({top: "-360%", opacity: 0}, 300);
+      $(origin.item).find(".hd h5 .left").animate({left: "-360%", opacity: 0}, 300);
+      $(origin.item).find(".hd h5 .right").animate({right: "-360%", opacity: 0}, 300);
       $(origin.item).find(".ft .btn-square").animate({top: "500px", opacity: 0}, 200);
 
       // 预算页 #budget
@@ -123,22 +161,55 @@ $(function() {
       // 免费服务 #service
       if(origin.index === 5) {
         // $("#service .hd h2").animate({top: "-360%", opacity: 0}, 300);
+        $(origin.item).find(".bd-list .list-item:nth-child(2n-1)").css("transform", "translateY(50%)").find(".txt").animate({top: "50%"}, 300);
+        $(origin.item).find(".bd-list .list-item:nth-child(2n)").css("transform", "translateY(-50%)").find(".txt").animate({top: "0%"}, 300);
       }
 
+      // 资深设计师一对一服务 #designer
+      if(origin.index === 6) {}
+
       // 额外服务支持 #support
-      if(origin.index === 6) {
-        $(origin.item).find(".list-item:nth-child(1)").delay(300).animate({opacity: 0}, 300, function() {
+      if(origin.index === 7) {
+        $(origin.item).find(".list-item:nth-child(1)").animate({opacity: 0}, 300, function() {
           $(this).css("transform", "translateX(150%)");
         });
-        $(origin.item).find(".list-item:nth-child(2)").delay(300).animate({opacity: 0}, 300, function() {
+        $(origin.item).find(".list-item:nth-child(2)").animate({opacity: 0}, 300, function() {
           $(this).css("transform", "translateX(50%)");
         });
-        $(origin.item).find(".list-item:nth-child(3)").delay(300).animate({opacity: 0}, 300, function() {
+        $(origin.item).find(".list-item:nth-child(3)").animate({opacity: 0}, 300, function() {
           $(this).css("transform", "translateX(-50%)");
         });
-        $(origin.item).find(".list-item:nth-child(4)").delay(300).animate({opacity: 0}, 300, function() {
+        $(origin.item).find(".list-item:nth-child(4)").animate({opacity: 0}, 300, function() {
           $(this).css("transform", "translateX(-150%)");
         });
+      }
+
+      // #slogan
+      if(origin.index === 8) {
+        $(origin.item).find(".bd-list .list-item").animate({left: "1200px"}, 300);
+        $(origin.item).find(".ft").animate({top: "500px", opacity: 0}, 300);
+      }
+
+      // #focus
+      if(origin.index === 9) {
+        $(origin.item).find(".bd img").animate({opacity: 0}, 300);
+        $(origin.item).find(".bd .bd-list.left li").animate({right: "1500px", opacity: 0}, 300);
+        $(origin.item).find(".bd .bd-list.right li").animate({left: "1500px", opacity: 0}, 300);
+      }
+
+      // #goal
+      if(origin.index === 10) {
+        $(origin.item).find(".bd-list").animate({height: "0px"}, 300);
+      }
+      
+      // #partner
+      if(origin.index === 11) {
+        $(origin.item).find(".bd-list").animate({opacity: 0}, 300);
+      }
+
+      // #about
+      if(origin.index === 12) {
+        $(origin.item).find(".cpy-des span").animate({opacity: 0, top: "500px"}, 300);
       }
 
     }
